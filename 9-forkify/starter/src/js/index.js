12c4143e8466f32a145117c23c6788620d69b16a
Johnny Chan <johchan24@gmail.com>
2019-08-12 17:40:37 +1000

    created recipe webapp

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,9 @@
 // Global app controller
+// import Search from './models/Search';
+import Search from './models/Search'
+
+const state = {}
+
+const search = new Search('chicken');
+console.log(search);
+search.getResult();
---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query
+  }
+
+  async getResult() {
+    const key = '48e32e2ca002797f6c524537c6930a86';
+    try {
+      const result = await axios(`https://www.food2fork.com/api/search?key=${key}&${this.query}`);
+      this.result = result.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
+
